f44965a207effabb9bc26efaae43030e9ccc4d71
Ayesha Aamir <59921536+ayeshaaamir@users.noreply.github.com>
2020-07-16 14:35:22 +0500

    Update speech recognition.py

--- speech recognition.py
+++ speech recognition.py
@@ -2,5 +2,12 @@
 import speech_recognition as sr
 record=sr.Recognizer()
 with sr.Microphone() as source:
-  record.adjust_ambient_noise(source)
-  
+  record.adjust_for_ambient_noise(source)
+  print("speak....")
+  audio=record.listen(source)
+
+try:
+  print("text: " + recording.recognize_google(audio))
+except Exception as e:
+  print(e)
+
