f6acfab170bd33ea3148db9c03ecfdb0513963fc
Ayesha Aamir <59921536+ayeshaaamir@users.noreply.github.com>
2020-07-16 12:55:50 +0500

    Update RS.py

--- RS.py
+++ RS.py
@@ -1,3 +1,5 @@
 import numpy as np #for array computation
 from scipy.io import wavfile #to access .wav files
 import matplotlib.pyplot as plt
+
+frequency_samples,audio_signal=wavfile.read("C:/Users/Kiet k Maree/Downloads/audio/seriously.wav")
