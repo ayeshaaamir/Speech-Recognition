acfaba3aa5d48bd75bb0bde8219247d5a0a87620
Ayesha Aamir <ayeshaaamir38@gmail.com>
2020-07-21 14:23:06 +0500

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -1,8 +1,11 @@
 import cv2
-img=cv2.imread("C:/Users/Kiet k Maree/Pictures/Saved Pictures/OOP.jpg")
-print(img)
+img=cv2.imread(C:/Users/Kiet k Maree/Downloads/images/cat)
 print(type(img))
+cv2.imshow('cat image',img)
+cv2.waitKey(0)
 
-#show image
-cv2.imshow('oop image',img)
-cv2.waitkey(0)
+# to rotate
+h,w=img.shape[0:2]
+rotate_matrix=cv2.getRotateMatrix2D((w/5,h/3),45,.2) #zoom in/zoom out
+cv2.imshow('rotate image',rotate_matrix)
+cv2.waitKey(0)
