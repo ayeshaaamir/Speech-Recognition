0d391e544c81d38c2a0a9757342fa99b69027383
Ayesha Aamir <ayeshaaamir38@gmail.com>
2020-07-21 14:26:53 +0500

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -9,3 +9,8 @@ h,w=img.shape[0:2]
 rotate_matrix=cv2.getRotateMatrix2D((w/5,h/3),45,.2) #zoom in/zoom out
 cv2.imshow('rotate image',rotate_matrix)
 cv2.waitKey(0)
+
+# to visualize matrix --> warpAffine
+rotate_img=cv2.warpAffine(img,rotate_matrix,(w,h))
+cv2.imshow('rotate image',rotate_img)
+cv2.waitKey(0)
