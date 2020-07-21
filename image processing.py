3b305b962805d267b6a95cd2a507d3bf9a2b5b86
Ayesha Aamir <ayeshaaamir38@gmail.com>
2020-07-21 14:36:07 +0500

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -6,11 +6,20 @@ cv2.waitKey(0)
 
 # to rotate
 h,w=img.shape[0:2]
-rotate_matrix=cv2.getRotateMatrix2D((w/5,h/3),45,.2) #zoom in/zoom out
+rotate_matrix=cv2.getRotateMatrix2D((w/5,h/3),45,1) #zoom in/zoom out
 cv2.imshow('rotate image',rotate_matrix)
 cv2.waitKey(0)
 
 # to visualize matrix --> warpAffine
 rotate_img=cv2.warpAffine(img,rotate_matrix,(w,h))
 cv2.imshow('rotate image',rotate_img)
+cv2.waitKey(0)
+
+# to crop image
+SR=int(h*15)
+SC=int(w*15)
+ER=int(h*84)
+EC=int(w*84)
+cropped_img=img[SR:ER,SC:EC]
+cv2.imshow('crop image',cropped_img)
 cv2.waitKey(0)
