e8d8aa97d68b58a27d2146cde3b66338c0a8d65c
Ayesha Aamir <ayeshaaamir38@gmail.com>
2020-07-23 13:09:40 +0500

    Update fashion mnist.py

--- fashion mnist.py
+++ fashion mnist.py
@@ -3,3 +3,7 @@ from keras.datasets import fashion_mnist
 
 # access dataset
 (x_train,y_train),(x_test,y_test)=fashion_mnist.load_data()
+
+class_name=['T-shirt/top','Trouser','pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+
+
