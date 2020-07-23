736abb814c1824ed4dec6c596d51688dfaca26af
Ayesha Aamir <ayeshaaamir38@gmail.com>
2020-07-23 14:07:53 +0500

    Update fashion mnist.py

--- fashion mnist.py
+++ fashion mnist.py
@@ -5,5 +5,22 @@ from keras.datasets import fashion_mnist
 (x_train,y_train),(x_test,y_test)=fashion_mnist.load_data()
 
 class_name=['T-shirt/top','Trouser','pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+import numpy as np
+import matplotlib.pyplot as plt
+plt.figure()
+plt.imshow(x_train[6])
+plt.show()
+
+# model for training
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten
+model=Sequential(
+    [
+     # stack of layers
+     Flatten(input_shape=(28,28)) #single dim
+     Dense(128,activation=tf.nn.relu)
+     Dense(10,activation=tf.nn.softmax) #multi class classification
+    ]
+)
 
 
